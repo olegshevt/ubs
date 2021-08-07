1478b1bf52ffdff7a300774c9ca2e928c197e43c
Oleg Shevtsov <oleg_shevtsov@epam.com>
2021-08-07 16:47:06 +0200

    Updated list page

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,50 +1,57 @@
-import React from 'react';
-import { useActions } from '../../hooks/useActions';
-import { useTypedSelector } from '../../hooks/useTypedSelectors';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import s from './ListPage.module.css';
-import Preloader from '../../components/common/Preloader/Preloader';
+import React from "react";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelectors";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import s from "./ListPage.module.css";
+import Preloader from "../../components/common/Preloader/Preloader";
 
 const ListPage: React.FC = () => {
+  const { fetchMusic } = useActions();
+  const { error, data, loading } = useTypedSelector((state) => state.music);
+  const dispatch = useDispatch();
 
-    const { fetchMusic } = useActions();
-    const { error, data, loading } = useTypedSelector((state) => state.music);
-    const dispatch = useDispatch();
+  React.useEffect(() => {
+    fetchMusic();
+  }, []);
 
-    React.useEffect(() => {
-        fetchMusic();
-    }, []);
-
-
-    if (loading) {
-        return (
-            <Preloader />
-        )
-    } else {
-        return (
-            <div>
-                <h1>Music list</h1>
-                <table className={s.listTable}>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Price</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    {error && <h3>{error}</h3>}
-                    {loading && <Preloader />}
-                    <tbody>
-                        {!error && !loading && data.map(item => (
-                            <tr key={item.id}><td><Link className={s.rowLink} to={`/product/${item.id}`}>{item.id}</Link></td><td>{item.title}</td><td>{item.price}</td><td>{item.description}</td></tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-}
+  if (loading) {
+    return <Preloader />;
+  } else {
+    return (
+      <div>
+        <h1>Music list Important Staff</h1>
+        <table className={s.listTable}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          {error && <h3>{error}</h3>}
+          {loading && <Preloader />}
+          <tbody>
+            {!error &&
+              !loading &&
+              data.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Link className={s.rowLink} to={`/product/${item.id}`}>
+                      {item.id}
+                    </Link>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.price}</td>
+                  <td>{item.description}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
 
 export default ListPage;
