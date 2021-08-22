4b5ba9be7fd525a72755d98f8d6c120656d2f6fa
Oleg Shevtsov <oleg_shevtsov@epam.com>
2021-08-22 14:34:00 +0200

    Updated 404-error page

--- src/pages/Error404/Error404.tsx
+++ src/pages/Error404/Error404.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import s from './404.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import s from "./404.module.css";
+import { Link } from "react-router-dom";
 
 const Error404: React.FC = () => {
-    return (
-        <div>
-            <div className={s.errorContainer}>
-                <h1 className={s.errorItem}>Page not found</h1>
-                <p className={s.errorItem}>You can check <Link to="/">our main page</Link>!</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className={s.errorContainer}>
+        <h1 className={s.errorItem}>Page not found</h1>
+        <h2>Not found</h2>
+        <p className={s.errorItem}>
+          You can check <Link to="/">our main page</Link>!
+        </p>
+        <h3>We could not find</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Error404;
