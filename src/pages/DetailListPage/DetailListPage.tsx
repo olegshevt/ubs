2299bb4882a0013b11362b5318d48d08fb1b9acb
Oleg Shevtsov <oleg_shevtsov@epam.com>
2021-08-30 22:06:58 +0200

    Detail List Page Update

--- src/pages/DetailListPage/DetailListPage.tsx
+++ src/pages/DetailListPage/DetailListPage.tsx
@@ -1,56 +1,55 @@
-import React from 'react';
-import Preloader from '../../components/common/Preloader/Preloader';
-import { useTypedSelector } from '../../hooks/useTypedSelectors';
-import { useActions } from '../../hooks/useActions';
+import React from "react";
+import Preloader from "../../components/common/Preloader/Preloader";
+import { useTypedSelector } from "../../hooks/useTypedSelectors";
+import { useActions } from "../../hooks/useActions";
 import { useParams } from "react-router-dom";
-import s from '../ListPage/ListPage.module.css';
-import { Link } from 'react-router-dom';
+import s from "../ListPage/ListPage.module.css";
+import { Link } from "react-router-dom";
 
 interface ParamTypes {
-    id: string
+  id: string;
 }
 
 const DetailListPage: React.FC = () => {
+  const { fetchMusicItem } = useActions();
+  const { error, data, loading } = useTypedSelector((state) => state.musicItem);
+  let { id } = useParams<ParamTypes>();
 
-    const { fetchMusicItem } = useActions();
-    const { error, data, loading } = useTypedSelector((state) => state.musicItem);
-    let { id } = useParams<ParamTypes>();
+  React.useEffect(() => {
+    fetchMusicItem(id);
+  }, []);
 
-    React.useEffect(() => {
-        fetchMusicItem(id);
-    }, []);
-
-    if (loading) {
-        return (
-            <Preloader />
-        )
-    } else {
-        return (
-            <div>
-                <h1>{data.title}</h1>
-                <table className={s.listTable}>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Price</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{id}</td>
-                            <td>{data.title}</td>
-                            <td>{data.price}</td>
-                            <td>{data.description}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <p><Link to='/'>Back</Link></p>
-            </div>
-
-        )
-    }
-}
+  if (loading) {
+    return <Preloader />;
+  } else {
+    return (
+      <div>
+        <h1>{data.title}</h1>
+        <h2>Detail List Page Edits</h2>
+        <table className={s.listTable}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{id}</td>
+              <td>{data.title}</td>
+              <td>{data.price}</td>
+              <td>{data.description}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>
+          <Link to="/">Back</Link>
+        </p>
+      </div>
+    );
+  }
+};
 
 export default DetailListPage;
